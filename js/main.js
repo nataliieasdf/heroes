202773a59bf86ef36d42a4dc60ccdafa1ede876e
Graphic <graphic@SOD-SJH-516-04M.local>
2015-10-16 14:32:33 -0400

    trying to display next hero

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+var curHero = $('img').hasClass('shown');
+var nextHero = $(curHero).next();
+
 $(document).ready(function(){
 
 	var heroName = $('img').attr('title');
@@ -5,4 +8,10 @@ $(document).ready(function(){
 	$('img:first-child').addClass('shown');
 	$('h1').text(heroName);
 
+	$('button.next').on('click', function(){
+
+		$(curHero).removeClass('shown');
+		$(nextHero).addClass('shown');
+
+	});
 });
