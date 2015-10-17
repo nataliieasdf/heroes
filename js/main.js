59beadc1a09b3f44d03ab4a4b94fdf4ffa018bab
Natalie Shum <natalieshum@Nats-Mac.local>
2015-10-17 02:35:43 -0400

    small mods to put in previous button

--- js/main.js
+++ js/main.js
@@ -3,7 +3,8 @@ $(document).ready(function(){
 	var $currHero = $('img:first-child').addClass('shown');
 
 	$('.name').text( $('.shown').attr('title') );
-
+	//there must be a better way to display name 
+	//and then change later??
 
 	$('button.next').on('click', function(){
 
@@ -12,7 +13,18 @@ $(document).ready(function(){
 
 		var $heroName = $('img.shown').attr('title');
 		$('.name').text($heroName);
-		
+
 	});
+
+	$('button.prev').on('click', function(){
+
+		$('.shown').prev().addClass('shown');
+		$('.shown').next().removeClass('shown');
+
+		var $heroName = $('img.shown').attr('title');
+		$('.name').text($heroName);
+
+	});
+
 
 });
