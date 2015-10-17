15cc1b4e6d7c40a6440f53ddd7718f6f69d35abe
Natalie Shum <natalieshum@Nats-Mac.local>
2015-10-17 02:32:04 -0400

    figured out needed to target class.shown!

--- js/main.js
+++ js/main.js
@@ -1,16 +1,18 @@
 $(document).ready(function(){
-	var $currHero = $('img').first().addClass('shown');
-	var $heroName = $currHero.attr('title');
 
-	$('h1').text($heroName);
+	var $currHero = $('img:first-child').addClass('shown');
+
+	$('.name').text( $('.shown').attr('title') );
+
 
 	$('button.next').on('click', function(){
-		var $nextHero = $currHero.next();
-		if ( $currHero.hasClass('shown') )
 
-			$currHero.removeClass('shown');
-			// $nextHero.addClass('shown');
+		$('.shown').next().addClass('shown');
+		$('.shown').prev().removeClass('shown');
 
+		var $heroName = $('img.shown').attr('title');
+		$('.name').text($heroName);
+		
 	});
 
 });
